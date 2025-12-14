f14582ff06918d7c718e4994d7649733c36aded6
Danny Armijos <daniel2667@hotmail.es>
2025-12-14 23:48:00 +0100

    feat: agregar soporte de paginación y campo 'available' en el modelo de productos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from '../prisma.service';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -12,19 +13,51 @@ export class ProductsService {
     });
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(pagination: PaginationDto) {
+    const totalPage = await this.prismaService.product.count({
+      where: { available: true },
+    });
+    const lastPage = Math.ceil(totalPage / (pagination.limit || 10));
+    return {
+      data: await this.prismaService.product.findMany({
+        where: { available: true },
+        skip: ((pagination.page ?? 1) - 1) * 10,
+        take: pagination.limit,
+      }),
+      meta: {
+        totalItems: totalPage,
+        page: pagination.page || 1,
+        lastPage: lastPage,
+      },
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.prismaService.product.findUnique({
+      where: { id, available: true },
+    });
+    if (!product) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+    return await this.prismaService.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    await this.findOne(id);
+    return await this.prismaService.product.update({
+      where: { id },
+      data: { available: false },
+    });
+    // return await this.prismaService.product.delete({
+    //   where: { id },
+    // });
   }
 }
